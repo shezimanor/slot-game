1719696379cd032a2edfd326a94e4ac6ad8f40f5
Ryan Chen <y.f.chenroderick@gmail.com>
2025-04-29 00:23:11 +0800

    chore: 建立按鈕互動測試

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,14 +1,15 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Button, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
-    start() {
+  @property(Button)
+  public spinButton: Button = null;
 
-    }
-
-    update(deltaTime: number) {
-        
-    }
+  onClickSpin() {
+    this.spinButton.interactable = false;
+    this.scheduleOnce(() => {
+      this.spinButton.interactable = true;
+    }, 1);
+  }
 }
-
