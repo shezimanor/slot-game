332835d8862d27085118b8dc85ba5e9f4d10773f
Ryan Chen <y.f.chenroderick@gmail.com>
2025-05-03 14:21:56 +0800

    feat: 定義 SpinResult 介面

--- assets/scripts/types/index.d.ts
+++ assets/scripts/types/index.d.ts
@@ -20,7 +20,7 @@ export enum SlotSpriteFrameType {
   Scatter = 'item-scatter' // 7
 }
 
-type PayoutTable = {
+export type PayoutTable = {
   [key in SlotType]: {
     3: number;
     4: number;
@@ -28,9 +28,17 @@ type PayoutTable = {
   };
 };
 
-interface PayLineResult {
+export interface PayLineResult {
   lineIndex: number; // 第幾條賠付線（0~9）
   matched: SlotType; // 是哪個符號中獎（例：SlotType.Cherry）
   count: number; // 連線個數（3,4,5）
   win: number; // 該線贏得的金額
+}
+
+export interface SpinResult {
+  matrix: SlotType[][];
+  payLineResult: PayLineResult[];
+  totalWin: number;
+  scatterCount: number;
+  scatterTriggered: boolean;
 }
